de568db7849acc07aa25b0a6b4bf2adf24c7c84a
SANGISHETTISRIDHAR <dhayakar.dhaya235@gmail.com>
2019-11-13 22:29:46 +0530

    sridhar model created

--- gitwithdjango_app/models.py
+++ gitwithdjango_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Sridhar(models.Model):
+    sid=models.IntegerField(primary_key=True)
+    sname=models.CharField(max_length=50)
+    saddrs=models.CharField(max_length=50)
